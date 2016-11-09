29114b6ccf8b9a9ce068dd2c901e25ea35a9ab28
r48patel <r48patel@gmail.com>
2016-11-08 22:52:32 -0600

    Adding project folder in public

--- index.js
+++ index.js
@@ -1,38 +1,36 @@
 var express = require('express');
 var app = express();
-// var fs = require('fs');
-// var sleep = require('sleep');
+var fs = require('fs');
 
-// fs.readFile(__dirname + '/views/test.txt', 'utf8', function(err, data){
-// 	if (err) {
-// 		return console.log(err);
-// 	}
-// 	sleep.sleep(1)
-// 	// console.log(data)
-// });
+var gallery_info=[]
+var projects_info = fs.readFileSync(__dirname + '/public/projects.txt').toString().split('\n');
 
-// var file_gallery_info = fs.readFileSync(__dirname + '/views/test.txt').toString();
-// console.log('test: ' + file_gallery_info)
-// console.log(typeof file_gallery_info)
+for(i in projects_info){ 
+  var file_name = __dirname + '/public/projects/' + projects_info[i]
+  console.log('file_name: ' + file_name);
+  var project_file = fs.readFileSync().toString().split('\n')
+  // for(j in project_file){
+  //   console.log(project_file[j])
+  // }
+}
 
-
-var gallery_info = [
-  {
-  	title: 'Vancouver', sub_title: 'Summer 2016', 
-    images: {
-  		thumb:'http://tinyurl.com/jason-one-jpg',
-  		desc:'Jason Posing',
-      thumb_width: 285
-  	}
-  },
-  {
-    title: 'Seattle', sub_title: 'Summer 2016', images: {
-      thumb:'http://tinyurl.com/gnr7s6g',
-      desc:'Andrew Posing',
-      thumb_width: 285
-    }
-  }
-];
+// var gallery_info = [
+//   {
+//   	title: 'Vancouver', sub_title: 'Summer 2016', 
+//     images: {
+//   		thumb:'http://tinyurl.com/jason-one-jpg',
+//   		desc:'Jason Posing',
+//       thumb_width: 285
+//   	}
+//   },
+//   {
+//     title: 'Seattle', sub_title: 'Summer 2016', images: {
+//       thumb:'http://tinyurl.com/gnr7s6g',
+//       desc:'Andrew Posing',
+//       thumb_width: 285
+//     }
+//   }
+// ];
 
 app.set('port', (process.env.PORT || 5000));
 
