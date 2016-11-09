5ad36fa51e8c27a8a33eb804c296761b64d7b06d
r48patel <r48patel@gmail.com>
2016-11-09 14:55:57 -0600

    template.txt;Coming Soon for non existing projects

--- index.js
+++ index.js
@@ -5,31 +5,46 @@ var fs = require('fs');
 var gallery_info=[];
 var projects_info = fs.readFileSync(__dirname + '/public/projects.txt').toString().split('\n');
 
-for(var repeat=0; repeat<25;repeat++){
+for(var repeat=0; repeat<10;repeat++){
 	for(i in projects_info){ 
-		var file_name = __dirname + '/public/projects/' + projects_info[i];
-	  	var project_file = fs.readFileSync(file_name).toString().split('\n');
-		var images=[];
+		var file_name = __dirname + '/public/projects/' + projects_info[i] + '.txt';
+		if(fs.existsSync(file_name)) {
+			var project_file = fs.readFileSync(file_name).toString().split('\n');
+			var images=[];
 
-	  	for(var j=1; j<project_file.length;j++){
-			//img_url;img_msg
-			var picture_info = project_file[j].split(';');
-			images.push({
-				url: picture_info[0],
-				name: picture_info[1],
-				msg: picture_info[2]
-			});
-	  	};
+		  	for(var j=1; j<project_file.length;j++){
+				//img_url;img_msg
+				var picture_info = project_file[j].split(';');
+				images.push({
+					url: picture_info[0],
+					name: picture_info[1],
+					msg: picture_info[2]
+				});
+		  	};
 
-	  	//title;sub_title;thumb;thumb_width
-		var project_info = project_file[0].split(';');
-	  	gallery_info.push({
-	  		title: project_info[0],
-	  		sub_title: project_info[1],
-	  		thumb: project_info[2],
-	  		thumb_width: project_info[3],
-	  		images: images
-	  	});
+		  	//title;sub_title;thumb;thumb_width
+			var project_info = project_file[0].split(';');
+		  	gallery_info.push({
+		  		title: project_info[0],
+		  		sub_title: project_info[1],
+		  		thumb: project_info[2],
+		  		thumb_width: project_info[3],
+		  		images: images
+		  	});
+		}
+		else {
+			gallery_info.push({
+				title: projects_info[i],
+				sub_title:'Coming Soon!',
+				thumb: 'http://www.booktrip4me.com/Content/images/Coming-Soon.png',
+				thumb_width: '285',
+				images: [{
+					url: 'http://www.booktrip4me.com/Content/images/Coming-Soon.png',
+					name: 'Coming Soon',
+					msg: 'Coming Soon'
+				}]
+			})
+		}
 	};
 }
 
