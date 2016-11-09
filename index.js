aa349bf23bb828a1a037439a8af6547a88671bd0
r48patel <r48patel@gmail.com>
2016-11-09 00:14:31 -0600

    Dynamic page generation based on text files works

--- index.js
+++ index.js
@@ -2,35 +2,35 @@ var express = require('express');
 var app = express();
 var fs = require('fs');
 
-var gallery_info=[]
+var gallery_info=[];
 var projects_info = fs.readFileSync(__dirname + '/public/projects.txt').toString().split('\n');
 
-for(i in projects_info){ 
-  var file_name = __dirname + '/public/projects/' + projects_info[i]
-  console.log('file_name: ' + file_name);
-  var project_file = fs.readFileSync().toString().split('\n')
-  // for(j in project_file){
-  //   console.log(project_file[j])
-  // }
-}
+for(var repeat=0; repeat<25;repeat++){
+	for(i in projects_info){ 
+		var file_name = __dirname + '/public/projects/' + projects_info[i];
+	  	var project_file = fs.readFileSync(file_name).toString().split('\n');
+		var images=[];
 
-// var gallery_info = [
-//   {
-//   	title: 'Vancouver', sub_title: 'Summer 2016', 
-//     images: {
-//   		thumb:'http://tinyurl.com/jason-one-jpg',
-//   		desc:'Jason Posing',
-//       thumb_width: 285
-//   	}
-//   },
-//   {
-//     title: 'Seattle', sub_title: 'Summer 2016', images: {
-//       thumb:'http://tinyurl.com/gnr7s6g',
-//       desc:'Andrew Posing',
-//       thumb_width: 285
-//     }
-//   }
-// ];
+	  	for(var j=1; j<project_file.length;j++){
+			//img_url;img_msg
+			var picture_info = project_file[j].split(';');
+			images.push({
+				url: picture_info[0],
+				msg: picture_info[1]
+			});
+	  	};
+
+	  	//title;sub_title;thumb;thumb_width
+		var project_info = project_file[0].split(';');
+	  	gallery_info.push({
+	  		title: project_info[0],
+	  		sub_title: project_info[1],
+	  		thumb: project_info[2],
+	  		thumb_width: project_info[3],
+	  		images: images
+	  	});
+	};
+}
 
 app.set('port', (process.env.PORT || 5000));
 
