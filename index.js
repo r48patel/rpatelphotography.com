78841777091ba179ac51664e81050eeaa5d1867d
r48patel <r48patel@gmail.com>
2017-03-17 14:28:22 -0500

    Restructure portfolio files; Add CNE and Disney; Add Exclude logic

--- index.js
+++ index.js
@@ -4,14 +4,14 @@ var fs = require('fs');
 var path = require('path');
 var logger = require('morgan');
 
-var gallery_info=[];
 
-function update_gallery_info() {
-	var projects_info = fs.readFileSync(path.join(__dirname,'views','templates','projects.txt')).toString().split('\n');
-	gallery_info=[];
+function get_portfolio_info() {
+	var exclude_projects = ['Disney'];
+	var projects_info = fs.readdirSync(path.join(__dirname,'views','portfolio'));
+	var portfolio_info=[];
 	for (var i = 0; i < projects_info.length; i++) {
-		var file_name = __dirname + '/views/templates/projects/' + projects_info[i];
-		if (fs.existsSync(file_name)) {
+		var file_name = __dirname + '/views/portfolio/' + projects_info[i];
+		if (exclude_projects.indexOf(projects_info[i]) < 0 ) {
 			console.log('Reading: ' + file_name);
 			var project_file = fs.readFileSync(file_name).toString().split('\n');
 			var images = [];
@@ -25,7 +25,7 @@ function update_gallery_info() {
 
 			//title;sub_title;thumb
 			var project_info = project_file[0].split(';');
-			gallery_info.push({
+			portfolio_info.push({
 				title: project_info[0],
 				sub_title: project_info[1],
 				thumb: project_info[2],
@@ -33,19 +33,15 @@ function update_gallery_info() {
 			});
 		}
 		else {
-			console.log("Can't find: " + file_name);
-			gallery_info.push({
-				title: projects_info[i].split('.')[0],
-				sub_title: 'Coming Soon!',
-				thumb: 'http://www.booktrip4me.com/Content/images/Coming-Soon.png',
-				images: ['http://www.booktrip4me.com/Content/images/Coming-Soon.png']
-			})
+			console.log('Excluding: ' + file_name)
 		}
 	}
+
+	return portfolio_info
 }
 
-function does_blog_exists(gallery_requsted) {
-	return fs.existsSync(path.join('views','blogs', gallery_requsted + '.ejs'));
+function does_blog_exists(portfolio_requsted) {
+	return fs.existsSync(path.join('views','blogs', portfolio_requsted + '.ejs'));
 }
 
 function get_blog_list(){
@@ -63,13 +59,12 @@ app.set('view engine', 'ejs');
 
 app.use(logger());
 app.get('/', function(request, response) {
-	update_gallery_info();
 	response.render('pages/index', {
 		title: 'Portfolio',
-	  	sub_title: 'Sharing some of the moments captured on my travels',
-		gallery_info: gallery_info,
+		sub_title: 'Sharing some of the moments captured on my travels',
+		portfolio_info: get_portfolio_info(),
 		blog_info: get_blog_list()
-  	});
+	});
 });
 
 app.post('/', function(request, response) {
@@ -84,34 +79,33 @@ app.get('/blog/:name', function(request, response){
 	var blogReq = request.params.name;
 
 	response.render('blogs/' + blogReq, {
-		gallery_info: gallery_info,
+		portfolio_info: portfolio_info,
 		blog_info: get_blog_list()
 	})
 });
 
-app.get('/gallery/:name', function(request, response) {
-  	var name = request.params.name;
-	update_gallery_info();
+app.get('/portfolio/:name', function(request, response) {
+	var name = request.params.name;
+	portfolio_info = get_portfolio_info();
 	if (name != 'favicon.ico'){
-    	for(var i=0;i<gallery_info.length;i++) {
-      		if(gallery_info[i].title == name) {
-        		response.render('pages/blog', {
-					title: gallery_info[i]['title'],
-					sub_title: gallery_info[i]['sub_title'],
-					thumb: gallery_info[i]['thumb'],
-					main: false,
+		for(var i=0;i<portfolio_info.length;i++) {
+			if(portfolio_info[i].title == name) {
+				response.render('pages/blog', {
+					title: portfolio_info[i]['title'],
+					sub_title: portfolio_info[i]['sub_title'],
+					thumb: portfolio_info[i]['thumb'],
 					blog_exists: does_blog_exists(name),
-					images: gallery_info[i]['images'],
-					gallery_info: gallery_info,
+					images: portfolio_info[i]['images'],
+					portfolio_info: portfolio_info,
 					blog_info: get_blog_list()
-        		});
-      	}
-    }
-  }
+				});
+			}
+		}
+	}
 });
 
 app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+	console.log('Node app is running on port', app.get('port'));
 });
 
 
