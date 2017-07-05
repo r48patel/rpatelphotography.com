eef1707d0b7670bea1607ccabf51d20cf9118ec8
Ravi Patel <r48patel@gmail.com>
2017-07-04 23:09:21 -0500

    Multi level portfolio works

--- index.js
+++ index.js
@@ -4,36 +4,69 @@ var fs = require('fs');
 var path = require('path');
 var logger = require('morgan');
 
+// Which projects to exclud
+var exclude_projects = ['Disney', '.DS_Store'];
 
-function get_portfolio_info() {
-	var exclude_projects = ['Disney'];
-	var projects_info = fs.readdirSync(path.join(__dirname,'views','portfolio'));
+var portfolio_details = {}
+
+function read_portfolio_file(file_name){
+	// console.log('Reading: ' + file_name);
+	
+	var project_file = fs.readFileSync(file_name).toString().split('\n');
+	var images = [];
+
+	for (var j = 1; j < project_file.length; j++) {
+		//img_url
+		if (!project_file[j].startsWith('#')) {
+			images.push(project_file[j]);
+		}
+	}
+
+	//title;sub_title;thumb
+	var project_info = project_file[0].split(';');
+	var info = {
+		title: project_info[0],
+		sub_title: project_info[1],
+		thumb: project_info[2],
+		link: 'portfolio/'+project_info[0],
+		images: images
+	};
+
+	portfolio_details[project_info[0]] = info
+
+	return info
+}
+
+function get_portfolio_info(location) {
+	if (typeof location === 'undefined') {
+		location = path.join(__dirname,'views','portfolio')
+	}
+
+	// console.log(location)
+
+	var projects_info = fs.readdirSync(path.join(location));
 	var portfolio_info=[];
 	for (var i = 0; i < projects_info.length; i++) {
-		var file_name = __dirname + '/views/portfolio/' + projects_info[i];
+		var file_name = path.join(location, projects_info[i]);
 		if (exclude_projects.indexOf(projects_info[i]) < 0 ) {
-			console.log('Reading: ' + file_name);
-			var project_file = fs.readFileSync(file_name).toString().split('\n');
-			var images = [];
-
-			for (var j = 1; j < project_file.length; j++) {
-				//img_url
-				if (!project_file[j].startsWith('#')) {
-					images.push(project_file[j]);
-				}
+			if(fs.lstatSync(file_name).isDirectory()){
+				// console.log(file_name + ' is a dir');
+				multi_info = get_portfolio_info(file_name)
+				// console.log(file_name.split("/").pop())
+				portfolio_info.push({
+					title: file_name.split("/").pop(),
+					sub_title: 'Various',
+					thumb: multi_info[0]['thumb'],
+					link: '/' + file_name.split("/").pop()
+				})
 			}
-
-			//title;sub_title;thumb
-			var project_info = project_file[0].split(';');
-			portfolio_info.push({
-				title: project_info[0],
-				sub_title: project_info[1],
-				thumb: project_info[2],
-				images: images
-			});
+			else {
+				info = read_portfolio_file(file_name)
+				portfolio_info.push(info)
+			}
 		}
 		else {
-			console.log('Excluding: ' + file_name)
+			// console.log('Excluding: ' + file_name)
 		}
 	}
 
@@ -66,6 +99,18 @@ app.get('/', function(request, response) {
 		blog_info: get_blog_list()
 	});
 });
+app.get('/:name', function(request, response) {
+	var multiPortfolio = request.params.name;
+	// console.log(multiPortfolio);
+	info = get_portfolio_info(path.join(__dirname,'views','portfolio', multiPortfolio))
+	// console.log(info)
+	response.render('pages/index', {
+		title: multiPortfolio,
+		sub_title: 'Various pictures from ' + multiPortfolio,
+		portfolio_info: info,
+		blog_info: get_blog_list()
+	});
+});
 
 // app.post('/', function(request, response) {
 // 	console.log(request);
@@ -87,20 +132,19 @@ app.get('/blog/:name', function(request, response){
 app.get('/portfolio/:name', function(request, response) {
 	var name = request.params.name;
 	portfolio_info = get_portfolio_info();
+	// console.log(portfolio_details)
+	asking_portfolio = portfolio_details[name];
+
 	if (name != 'favicon.ico'){
-		for(var i=0;i<portfolio_info.length;i++) {
-			if(portfolio_info[i].title == name) {
-				response.render('pages/blog', {
-					title: portfolio_info[i]['title'],
-					sub_title: portfolio_info[i]['sub_title'],
-					thumb: portfolio_info[i]['thumb'],
-					blog_exists: does_blog_exists(name),
-					images: portfolio_info[i]['images'],
-					portfolio_info: portfolio_info,
-					blog_info: get_blog_list()
-				});
-			}
-		}
+		response.render('pages/portfolio', {
+			title: asking_portfolio['title'],
+			sub_title: asking_portfolio['sub_title'],
+			thumb: asking_portfolio['thumb'],
+			blog_exists: does_blog_exists(name),
+			images: asking_portfolio['images'],
+			portfolio_info: portfolio_info,
+			blog_info: get_blog_list()
+		});
 	}
 });
 
